dc19f9102de2215a198f3a4e999a66b5501e9143
Zeph LaFassett <zeph@lafassett.com>
2013-08-25 14:52:09 -0700

    added paddle entity

---
+++ catchr-core/src/org/doublelong/catchr/entity/Paddle.java
@@ -0,0 +1,14 @@
+package org.doublelong.catchr.entity;
+
+import com.badlogic.gdx.physics.box2d.World;
+
+public class Paddle
+{
+
+	private final World world;
+
+	public Paddle(World world)
+	{
+		this.world = world;
+	}
+}
--- catchr-core/src/org/doublelong/catchr/screens/CatchrScreen.java
+++ catchr-core/src/org/doublelong/catchr/screens/CatchrScreen.java
@@ -1,14 +1,30 @@
 package org.doublelong.catchr.screens;
 
 import org.doublelong.catchr.Catchr;
+import org.doublelong.catchr.entity.Paddle;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
 
 public class CatchrScreen implements Screen
 {
+	private final World world;
+	private final OrthographicCamera cam;
+	private final Box2DDebugRenderer debugRenderer;
+
+	private final Paddle player;
 
 	public CatchrScreen(Catchr game, boolean debug)
 	{
+		this.world = new World(new Vector2(0f, -10f), false);
+		this.cam = new OrthographicCamera();
+		this.debugRenderer = new Box2DDebugRenderer();
+		this.player = new Paddle(this.world);
 
 	}
 
@@ -36,7 +52,7 @@ public class CatchrScreen implements Screen
 	@Override
 	public void render(float arg0)
 	{
-		// TODO Auto-generated method stub
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 
 	}
 
